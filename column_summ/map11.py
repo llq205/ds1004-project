92e0226e694a93c9d1ae128b48fa35122e730710
Yidi Zhang <yz3464@login-1-1.local>
2017-04-14 21:11:56 -0400

    upload column summary code

---
+++ column_summ/map11.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+#map function for task 1
+
+import sys
+import string
+import csv
+
+key = ''
+picked_column = None
+type = 'string'
+label = None
+descr = 'Progess'
+lines = csv.reader(sys.stdin)
+
+for entry in lines:
+    key = entry[0]
+    picked_column = entry[10]
+    if picked_column == None or picked_column == 'N/A' or picked_column == '':
+        label = 'NULL'
+        type = 'NULL'
+       # print picked_column
+    else:
+        try:
+            picked_column = str(picked_column)
+            label = 'VALID'
+        except ValueError:
+            label = 'INVALID'
+    #type = picked_column.type
+    print '%s,%s,%s,%s' % (key,type,descr,label)
